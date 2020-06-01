c3fabdb59c26bf2d160e4de96ede0fefc04d4f3c
MathiasMen <mathiasmentler@web.de>
2020-06-01 16:05:13 +0200

    Added further processing of address-field.

--- clean_adresses.py
+++ clean_adresses.py
@@ -1,6 +1,6 @@
 import csv
 import json
-
+import re
 
 db_input_path = "/Users/mathias/Documents/programming_workspace/DataViz/input/db.json"
 db_output_path = "/Users/mathias/Documents/programming_workspace/DataViz/output/db_cleaned.csv"
@@ -9,11 +9,13 @@ with open(db_input_path) as infile:
     db = json.loads(infile.read())
 
 db_data = db["RealEstates"]
+regex = re.compile(".*\|")
 
 with open(db_output_path, "w") as outfile:
     f = csv.writer(outfile)
     f.writerow(["link", "address", "price", "additional_cost", "space"])
     for x in range(0,len(db_data)):
 	    d = db_data[x]
-	    add = d["address"].replace(" Die vollständige Adresse der Immobilie erhalten Sie vom Anbieter.","")
-	    f.writerow([d["link"], add, d["price"], d["additional_cost"], d["space"]])
+	    address = d["address"].replace(" Die vollständige Adresse der Immobilie erhalten Sie vom Anbieter.","")
+	    address = regex.sub("",address)
+	    f.writerow([d["link"], address, d["price"], d["additional_cost"], d["space"]])
